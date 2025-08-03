6bc4649869b1b99d6680a8c11bb20600868ded9e
Morialle Barr <Moriallekbarr@gmail.com>
2025-08-03 18:28:25 +0300

    Lesson 5.16 - 5.17

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] float collisionCooldown = 1;
+
+    const string hitString = "Hit";
+
+    float cooldownTimer = 0f;
+
+    void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+    }
+
     void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.name);
+        if (cooldownTimer < collisionCooldown) return;
+
+        animator.SetTrigger(hitString);
+        cooldownTimer = 0f;
     }
 }
