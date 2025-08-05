e0e46ff677842c249f6fa63dcb22e530a62c7032
Morialle Barr <Moriallekbarr@gmail.com>
2025-08-05 19:34:28 +0300

    Lessons 5.34 - 5.38

--- Assets/Scripts/Proc Gen/Checkpoint.cs
+++ Assets/Scripts/Proc Gen/Checkpoint.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     [SerializeField] float checkPointTimeExtension = 5f;
+    [SerializeField] float obstacleDecreaseTimeAmount = .2f;
 
     GameManager gameManager;
+    ObstacleSpawner obstacleSpawner;
 
     const string playerString = "Player";
 
     void Start()
     {
         gameManager = FindFirstObjectByType<GameManager>();
+        obstacleSpawner = FindFirstObjectByType<ObstacleSpawner>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,6 +21,7 @@ public class Checkpoint : MonoBehaviour
         if (other.CompareTag(playerString))
         {
             gameManager.IncreaseTime(checkPointTimeExtension);
+            obstacleSpawner.DecreaseObstacleSpawnTime(obstacleDecreaseTimeAmount);
         }
     }
 }
