ab00da27a6db1b4b784ea7b81c42d98dbfe845d1
Morialle Barr <Moriallekbarr@gmail.com>
2025-08-05 09:23:29 +0300

    Lesson 5.18

--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 
-public class Pickup : MonoBehaviour
+public abstract class Pickup : MonoBehaviour
 {
+    [SerializeField] float rotationSpeed = 100f;
+
     const string playerString = "Player";
+
+    void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerString))
         {
-            Debug.Log("Picked up by player");
+            OnPickup();
+            Destroy(gameObject);
         }
     }
+    
+    protected abstract void OnPickup();
 }
